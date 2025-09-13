b9970be6c425089f8de3502ce222ad47f4e8d6ec
karol rutkowski <karolrut123@gmail.com>
2025-09-13 13:34:13 +0200

    feat: end design part

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercelStatic from '@astrojs/vercel/static';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'static',
+  adapter: vercelStatic({})
+});
